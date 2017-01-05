ac664a1f2e0552d2b2cdaea5610f6f506c021e78
Jorge <Jorge>
2017-01-05 14:49:47 -0200

    first commit

---
+++ src/main/java/nl/berg/pack/selenium/App.java
@@ -0,0 +1,13 @@
+package nl.berg.pack.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
